cd8176067ca941c7a09a93d6a42852a710b61a9d
rbabreu <rbabreu@gmail.com>
2012-03-08 13:40:06 -0400

    adding testable singleton files

---
+++ ConnectionPool/ConnectionPool/ConnectionPool.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+
+namespace Singleton
+{
+    public class ConnectionPool
+    {
+        private static ConnectionPool _instance;
+        private ConnectionPool() { }
+        public static ConnectionPool Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new ConnectionPool();
+                }
+                return _instance;
+            }
+        }
+    }
+
+    public class TestableConnectionPool : ConnectionPool
+    {
+        public TestableConnectionPool()
+        {
+            FieldInfo fieldInfo = typeof(ConnectionPool).GetField("_instance", BindingFlags.Static | BindingFlags.NonPublic);
+            fieldInfo.SetValue(Instance, this);
+        }
+    }
+}
---
+++ ConnectionPool/Testable/TestableConnectionPool.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+namespace Singleton
+{
+    public class TestableConnectionPool : ConnectionPool
+    {
+        public TestableConnectionPool()
+        {
+            FieldInfo fieldInfo = typeof(ConnectionPool).GetField("_instance", BindingFlags.Static | BindingFlags.NonPublic);
+            fieldInfo.SetValue(Instance, this);
+        }
+    }
+}
